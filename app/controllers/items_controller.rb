c52bb8eaa6808ba3f177eaa340720eda0f9e66c1
C0deMaverick <jbluhm@seaton63.com>
2017-01-01 17:35:59 -0700

    added form and item model

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,21 @@
+class ItemsController < ApplicationController
+	
+	def index
+	end
+
+	def new 
+		@item = Item.new
+	end
+
+	def create
+		@item = Item.new(item_params)
+	end
+
+	private
+
+		def item_params
+			params.require(:item).permit(:title, :description, :maker)
+		end
+
+
+end
